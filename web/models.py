8e421cf6c78fb792511dee9875bd03a0a7472f63
Shayan <shayan.delbari@protonmail.com>
2024-03-26 14:47:06 -0300

    Added Income and made some modifications

--- web/models.py
+++ web/models.py
@@ -7,3 +7,16 @@ class Expense(models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.text} | {self.date}'
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f'{self.text} | {self.date}'
+
